bdc07bfb3c2e40a20bcaef1b6569284f4c3df437
Antonio Ruiz Rondán <toninoes@gmail.com>
2017-06-02 20:10:54 +0200

    Algunas correcciones en el helper del foro

--- app/helpers/forum_helper.rb
+++ app/helpers/forum_helper.rb
@@ -3,12 +3,12 @@ module ForumHelper
     content = ''
     for post in posts
       url = link_to("#{h post.subject}", {:action => 'show', :id => post.id})
-      button = button_to 'Delete', {:action => 'destroy', :method => 'post', :id => post },
-                                   data: { confirm: "Seguro que quieres borrar el post #{post.subject}?" }
+      button = button_to 'Borrar', {:action => 'destroy', :method => 'post', :id => post },
+                                   data: { confirm: "¿Seguro que quieres borrar el post: \"#{post.subject}\"?" }
       margin_left = post.depth*20
       content << %(
       <tr><td><div style="margin-left:#{margin_left}px">
-        #{url} by #{h post.name} &middot; #{post.created_at.strftime("%H:%M:%S %Y-%M-%d") }
+        #{url} escrito por #{h post.name} &middot; #{post.created_at.strftime("%H:%M:%S - %d/%M/%Y") }
       </div></td>)
       content << %(<td><div>#{button}</div><td>) unless post.parent_id != 0
       content << %(</tr>)
